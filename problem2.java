b7eff4dfc5fb7ccb2e016e229ef43836ba7c8290
ttdangg <ttdang1445@gmail.com>
2024-09-03 10:10:16 -0700

    added age

--- problem2.java
+++ problem2.java
@@ -1,1 +1,16 @@
+public class problem2 
+{
+    public static void main(String[] args)
+    {
+        //int, double, boolean, string 
+        int myAge = 999; // int is for whole numbers, integers 
+        double price = 4.99;
 
+        boolean isGood; // variable declaration 
+        isGood = false; // variable intialization 
+
+        String name = "Tricia";
+        System.out.println("Hello my name is " + name);
+        System.out.print("My age is " + myAge);
+    }
+}
